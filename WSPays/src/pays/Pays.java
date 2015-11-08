d1bf09ccc42891c3bade706d34691bd4c0cb5458
Laura <premillieu.laura@gmail.com>
2015-11-08 11:57:20 +0100

    connexion bdd

--- WSPays/src/pays/Pays.java
+++ WSPays/src/pays/Pays.java
@@ -1,50 +1,66 @@
 package pays;
 
+import javax.persistence.*;
+
+/**
+ * Created by Laura on 08/11/2015.
+ */
+@Entity
+@Table(name = "pays", schema = "", catalog = "mespays")
 public class Pays {
-	private String nom;
-	private String nomCapital;
-	private int nbHabitants;
-	
-	public Pays() {
-		this.nom = "Inconnue";
-		this.nomCapital = "Inconnue";
-		this.nbHabitants = 0;
-	}
-	
-	public Pays(String nom) {
-		this.nom = nom;
-		this.nomCapital = "Inconnue";
-		this.nbHabitants = 0;
-	}
+    private String nomPays;
+    private String nomCapitale;
+    private long nbHabitants;
 
-	public Pays(String nom, String nomCapital, int nbHabitants) {
-		this.nom = nom;
-		this.nomCapital = nomCapital;
-		this.nbHabitants = nbHabitants;
-	}
+    @Id
+    @Column(name = "NOM_PAYS")
+    public String getNomPays() {
+        return nomPays;
+    }
 
-	public String getNom() {
-		return nom;
-	}
+    public void setNomPays(String nomPays) {
+        this.nomPays = nomPays;
+    }
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	
-	public String getNomCapital() {
-		return nomCapital;
-	}
+    @Basic
+    @Column(name = "NOM_CAPITALE")
+    public String getNomCapitale() {
+        return nomCapitale;
+    }
 
-	public void setNomCapital(String nomCapital) {
-		this.nomCapital = nomCapital;
-	}
+    public void setNomCapitale(String nomCapitale) {
+        this.nomCapitale = nomCapitale;
+    }
 
-	public int getNbHabitants() {
-		return nbHabitants;
-	}
+    @Basic
+    @Column(name = "NB_HABITANTS")
+    public long getNbHabitants() {
+        return nbHabitants;
+    }
 
-	public void setNbHabitants(int nbHabitants) {
-		this.nbHabitants = nbHabitants;
-	}
+    public void setNbHabitants(long nbHabitants) {
+        this.nbHabitants = nbHabitants;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pays that = (Pays) o;
+
+        if (nbHabitants != that.nbHabitants) return false;
+        if (nomPays != null ? !nomPays.equals(that.nomPays) : that.nomPays != null) return false;
+        if (nomCapitale != null ? !nomCapitale.equals(that.nomCapitale) : that.nomCapitale != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = nomPays != null ? nomPays.hashCode() : 0;
+        result = 31 * result + (nomCapitale != null ? nomCapitale.hashCode() : 0);
+        result = 31 * result + (int) (nbHabitants ^ (nbHabitants >>> 32));
+        return result;
+    }
 }
